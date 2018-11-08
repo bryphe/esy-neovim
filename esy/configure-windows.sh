23be262b34662036cb4f0110f3ef714f631850cb
Bryan Phelps <bryphe@outlook.com>
2018-11-08 10:00:24 -0800

    Add azure ci

---
+++ esy/configure-windows.sh
@@ -0,0 +1,4 @@
+mkdir -p _build
+cd _build
+
+cmake -G "Unix Makefiles" ../ -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_LINKER=/usr/bin/x86_64-w64-mingw32-ld -DCMAKE_AR=/usr/bin/x86_64-w64-mingw32-ar -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
